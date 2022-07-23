69adc4c6624fe15ca332aefbe71e4244dff3c773
Ahmet Cengizhan Dirican <acdirican@gmail.com>
2022-07-23 18:14:55 +0300

    mysql import file updated

--- inventorymaster_jpa_db.sql
+++ inventorymaster_jpa_db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Anamakine: 127.0.0.1
--- Üretim Zamanı: 23 Tem 2022, 00:49:08
+-- Üretim Zamanı: 23 Tem 2022, 17:14:20
 -- Sunucu sürümü: 10.4.24-MariaDB
 -- PHP Sürümü: 7.4.28
 
@@ -38,6 +38,25 @@ CREATE DEFINER=`root`@`localhost` PROCEDURE `get_products_byquantity` (IN `quant
 END$$
 
 DELIMITER ;
+
+-- --------------------------------------------------------
+
+--
+-- Tablo için tablo yapısı `contact_person`
+--
+
+CREATE TABLE `contact_person` (
+  `FirstName` varchar(255) DEFAULT NULL,
+  `LastName` varchar(255) DEFAULT NULL,
+  `SupplierID` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Tablo döküm verisi `contact_person`
+--
+
+INSERT INTO `contact_person` (`FirstName`, `LastName`, `SupplierID`) VALUES
+('Mark', 'Zukerberk', 11);
 
 -- --------------------------------------------------------
 
@@ -87,22 +106,32 @@ INSERT INTO `product` (`ID`, `name`, `quantity`, `SupplierID`) VALUES
 
 CREATE TABLE `supplier` (
   `ID` int(11) NOT NULL,
-  `name` varchar(255) DEFAULT NULL
+  `name` varchar(255) DEFAULT NULL,
+  `address` varchar(255) DEFAULT NULL,
+  `phone` varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Tablo döküm verisi `supplier`
 --
 
-INSERT INTO `supplier` (`ID`, `name`) VALUES
-(2, 'Farma'),
-(3, 'Zanio Warehouse'),
-(4, 'Zamaron Co.'),
-(7, 'Ekol Lojistik');
+INSERT INTO `supplier` (`ID`, `name`, `address`, `phone`) VALUES
+(2, 'Farma', NULL, NULL),
+(3, 'Zanio Warehouse', NULL, NULL),
+(4, 'Zamaron Co.', NULL, NULL),
+(7, 'Ekol Lojistik', NULL, NULL),
+(10, '1', '3', '2'),
+(11, 'Kalmia Ataco Co.', 'Germany', '+45 852 41');
 
 --
 -- Dökümü yapılmış tablolar için indeksler
 --
+
+--
+-- Tablo için indeksler `contact_person`
+--
+ALTER TABLE `contact_person`
+  ADD PRIMARY KEY (`SupplierID`);
 
 --
 -- Tablo için indeksler `product`
@@ -131,11 +160,17 @@ ALTER TABLE `product`
 -- Tablo için AUTO_INCREMENT değeri `supplier`
 --
 ALTER TABLE `supplier`
-  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 
 --
 -- Dökümü yapılmış tablolar için kısıtlamalar
 --
+
+--
+-- Tablo kısıtlamaları `contact_person`
+--
+ALTER TABLE `contact_person`
+  ADD CONSTRAINT `FKrydgtetn9l9cs8qht5o9albr2` FOREIGN KEY (`SupplierID`) REFERENCES `supplier` (`ID`);
 
 --
 -- Tablo kısıtlamaları `product`
